5431e30d952fd015670e2558030f9e2ec266e0b0
QuocBaoo <quocbaoo.dev@gmail.com>
2024-04-08 15:00:53 +0700

    init source

---
+++ app.js
@@ -0,0 +1,13 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slider = document.querySelector('.slider');
+
+next.addEventListener('click', () => {
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+})
+
+prev.addEventListener('click', () => {
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+})
